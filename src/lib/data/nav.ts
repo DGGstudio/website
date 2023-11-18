f08694955dcdcd9affd272cdf92cc6692d8d28bb
DGG2021 <82855247+DGG2021@users.noreply.github.com>
2023-11-18 12:10:52 +0700

    Update nav.ts

--- src/lib/data/nav.ts
+++ src/lib/data/nav.ts
@@ -2,6 +2,8 @@ export const nav = {
   brand: 'Team V',
   logo: '/favicon.png',
   items: [
+    { label: 'Home', href: '/about' },
+    { lable: 'Games', href: '/games' },
     { label: 'About Us', href: '/about' },
     { label: 'Contact', href: '/contact' },
     { label: 'Privacy', href: '/privacy' },
